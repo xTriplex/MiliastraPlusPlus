a3e8e0dc978e1ddf0485c39c3a970c5bfadb1bfc
Kaan Ayyıldız(xTriplex) <kaan_xtriplex@hotmail.com>
2026-09-19 07:49:06 +0300

    Post-Phase 5: convert core to static library

--- Miliastra++/Source/Main.cpp
+++
@@ -1,9 +0,0 @@
-#include "mpppch.h"
-
-int main(int argc, char* argv[])
-{
-    std::cout << "Initializing Miliastra++ Node Graph Generator...\n";
-
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::cin.get();
-}
